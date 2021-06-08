a0b688f491065c8563f4308ae1ef30ed41e8c669
rockjeon <sangwook27@naver.com>
2021-06-08 15:45:14 +0900

    practice

--- filter/index.js
+++ filter/index.js
@@ -1,36 +1,67 @@
-const dataList = [
-    {name:'name1',age:18},
-    {name:'name2',age:27},
-    {name:'name3',age:32},
-    {name:'name4',age:41},
-    {name:'name5',age:51},
+// const dataList = [
+//     {name:'name1',age:18},
+//     {name:'name2',age:27},
+//     {name:'name3',age:32},
+//     {name:'name4',age:41},
+//     {name:'name5',age:51},
 
-]
-function showList() {
-    let list = ''
-    for (const data of dataList) {
-        list += `<li> ${data.name} :${data.age}</li>`
+// ]
+
+
+// function showList(filterdList) {
+//     if(filterdList) {
+//         let list = ''
+//             for(const data of filterdList){
+//                 list += `<li>${data.name}:${data.age}</li>`        
+//             }
+//             document.querySelector('.users-list').innerHTML = list
+//     } else {
+//         let list = ''
+//         for(const data of dataList){
+//             list += `<li>${data.name}:${data.age}</li>`        
+//         }
+//         document.querySelector('.users-list').innerHTML = list
+
+//     }
+// }
+// showList()
+
+// document.querySelectorAll('button').forEach(element =>{
+//     element.addEventListener('click',(event)=> {
+//         onClick(event)
+//     })
+// })
+
+// function onClick(event) {
+//     const targetAge = event.target.dataset.age
+//     const filterdList = dataList.filter((data)=> data.age >= targetAge)
+//     showList(filterdList)
+// }
+// const array = [['a','b','c'],['r','g']]
+// const flattenedArray = array.reduce((pre, cur) => {
+// 	return pre.concat(cur)
+// })
+// console.log(flattenedArray
+
+// const arr = [
+//     {id:10, name:'사자'},
+//     {id:1, name:'호랑이'},
+//     {id:2, name:'기린'}
+// ]
+
+function showCurrentDate(argument) {
+    if(argument instanceof Date) {
+        console.log(`현재는 ${argument.toLocaleString()}`)
+    } else {
+        console.log('diff')
     }
-    document.querySelector('.users-list').innerHTML = list
-}
-showList()
-
-document.querySelectorAll('button').forEach(el => {
-    el.addEventListener('click',(event) => {
-        onClickButton(event)
-    })
-})
-
-function onClickButton(event) {
-    const targetAge= event.target.dataset.age
-    const filterdList = dataList.filter((data)=> data.age >= targetAge )
-    updateList(filterdList)
 }
 
-function updateList(filterdList) {
-    let listHtml = ''
-    for(const data of filterdList) {
-        listHtml += `<li>${data.name} : ${data.age}</li>`
-    }
-    document.querySelector('.users-list').innerHTML = listHtml
-}
+const today = new Date()
+const err = 1 
+
+showCurrentDate(today)
+showCurrentDate(err)
+
+
+
