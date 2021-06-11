9cff4c26eea46b63833e270fbaaf57882d8dde99
rockjeon <sangwook27@naver.com>
2021-06-12 00:48:12 +0900

    json data practice

--- timeControl/index.js
+++ timeControl/index.js
@@ -1,8 +1,23 @@
-document.querySelector('form').addEventListener('change', ()=> {
-    const id = document.forms[0].elements[0].value
-    const email = document.forms[0].elements[1].value
+async function load() {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const obj = await data.json()
+    const userId = obj.filter(result => result.userId === 1)
+    show(userId)
+}
+
+function show(userId) {
+    userId.forEach(element => {
+        const id = element.id
+        const userId = element.userId
+        const title = element.title
+        const body = element.body
+        });
     
-})
+}
+load()
+
+
+
 
 
 
